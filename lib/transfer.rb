3701e7803dac560dfa92aa9f716580111626806c
Felice Forby <feliceforby@gmail.com>
2018-05-13 07:52:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
   
   def execute_transaction
-  if @sender.valid? && @sender.balance >= @amount && @status != "complete"
+  if self.valid? && @sender.balance >= @amount && @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
